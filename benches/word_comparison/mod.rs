97834463b60abc768bd5e5ec41a547697068da5b
Eldritch Cheese <eldritch.cheese@gmail.com>
2022-01-21 08:05:00 -0600

    Added benchmark for avoiding quadratic loop in word comparison.

--- benches/word_comparison/mod.rs
+++ benches/word_comparison/mod.rs
@@ -1,5 +1,6 @@
 mod bytearray_arr_to_arr;
 mod bytearray_class_to_class;
+mod bytearray_class_to_class_2pass;
 mod string_to_vector;
 
 use criterion::Criterion;
@@ -12,6 +13,10 @@ pub fn benchmark_group(c: &mut Criterion) {
     group.bench_function(
         "bytearray_class_to_class",
         bytearray_class_to_class::bench::<5>,
+    );
+    group.bench_function(
+        "bytearray_class_to_class_2pass",
+        bytearray_class_to_class_2pass::bench::<5>,
     );
     group.bench_function(
         "bytearray_arr_to_arr",
