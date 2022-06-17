a55f3f3fb188c0c549ab52c205619e7c8befd778
Eldritch Cheese <eldritch.cheese@gmail.com>
2022-06-17 08:52:54 -0400

    Added WorkOnWorst and MinimizeWorstHeuristc quordle strategies

--- src/wordle/strategy/multi_impls.rs
+++ src/wordle/strategy/multi_impls.rs
@@ -1,6 +1,12 @@
-use super::{MultiStrategy, Strategy};
+use std::collections::HashSet;
+
+use super::{HeuristicStrategy, MultiStrategy, Strategy};
 use crate::{Error, MultiGameState, Word};
 
+// Solve each puzzle in order.  Information gained when solving
+// earlier puzzles is tracked in order to apply to later puzzles, but
+// the first unsolved puzzle is considered when determining the next
+// guess.
 pub struct MultiSequential<S: Strategy<N>, const N: usize> {
     single: S,
 }
@@ -24,5 +30,90 @@ impl<S: Strategy<N>, const N: usize, const GAMES: usize> MultiStrategy<N, GAMES>
             .find(|game| !game.is_finished())
             .map(|game| self.single.make_guess(game))
             .ok_or(Error::NoWordsRemaining)??)
+    }
+}
+
+// When making a guess, first select the puzzle that is furthest from
+// being solved, then make the best guess for that puzzle.
+pub struct WorkOnWorst<S: Strategy<N>, const N: usize> {
+    single: S,
+}
+
+impl<S: Strategy<N>, const N: usize> WorkOnWorst<S, N> {
+    pub fn new(single: S) -> Self {
+        Self { single }
+    }
+}
+
+impl<S: Strategy<N>, const N: usize, const GAMES: usize> MultiStrategy<N, GAMES>
+    for WorkOnWorst<S, N>
+{
+    fn make_guess(
+        &self,
+        state: &MultiGameState<N, GAMES>,
+    ) -> Result<Word<N>, Error> {
+        state.find_concluding_guess().map_or_else(
+            || {
+                state
+                    .games
+                    .iter()
+                    .filter(|game| !game.is_finished())
+                    .min_by_key(|game| game.possible_secrets.len())
+                    .map(|game| self.single.make_guess(game))
+                    .unwrap()
+            },
+            |guess| Ok(guess),
+        )
+    }
+}
+
+//
+pub struct MinimizeWorstHeuristic<S: HeuristicStrategy<N>, const N: usize> {
+    single: S,
+}
+
+impl<S: HeuristicStrategy<N>, const N: usize> MinimizeWorstHeuristic<S, N> {
+    pub fn new(single: S) -> Self {
+        Self { single }
+    }
+}
+
+impl<S: HeuristicStrategy<N>, const N: usize, const GAMES: usize>
+    MultiStrategy<N, GAMES> for MinimizeWorstHeuristic<S, N>
+{
+    fn make_guess(
+        &self,
+        state: &MultiGameState<N, GAMES>,
+    ) -> Result<Word<N>, Error> {
+        state.find_concluding_guess().map_or_else(
+            || {
+                state
+                    .games
+                    .iter()
+                    .map(|game| game.allowed_guesses.iter())
+                    .flatten()
+                    .collect::<HashSet<_>>()
+                    .into_iter()
+                    .min_by_key(|guess| self.multi_heuristic(state, guess))
+                    .map(|x| *x)
+                    .ok_or(Error::NoWordsRemaining)
+            },
+            |guess| Ok(guess),
+        )
+    }
+}
+
+impl<S: HeuristicStrategy<N>, const N: usize> MinimizeWorstHeuristic<S, N> {
+    fn multi_heuristic<const GAMES: usize>(
+        &self,
+        state: &MultiGameState<N, GAMES>,
+        guess: &Word<N>,
+    ) -> S::Output {
+        state
+            .games
+            .iter()
+            .map(|game| self.single.heuristic(game, guess))
+            .max()
+            .unwrap()
     }
 }
